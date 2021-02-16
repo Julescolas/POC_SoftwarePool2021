426e06b2420639b5d349c3389270bbcf2d4ef91b
jules.colas@epitech.eu <jules.colas@epitech.eu>
2021-02-16 11:11:53 -0500

    exercice 02 v1

--- day02/src/server.ts
+++ day02/src/server.ts
@@ -1,9 +1,57 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import bodyParser from  'body-parser'
 var server = express();
 
-
+server.use(cookieParser());
+server.use(bodyParser.json());
 server.get('/hello', function(request, response) {
-    response.send("world");
+  response.send("world");
+});
+
+server.get('/repeat-my-query', function(request, response) {
+  if (request.query.message == null) {
+    response.send("Bad request");
+    response.status(400);
+  }
+  if (request.query.message) {
+      response.send(request.query.message);
+  }
+});
+
+server.get('repeat-my-param/:message', function(request, response) {
+  response.send(request.params.message);
+});
+
+server.post('/repeat-my-body', function(request, response) {
+  if (!request.body) {
+    response.send("Bad request");
+    response.status(400);
+  }
+  if (request.body) {
+    response.send(request.body);
+  }
+});
+
+server.get('/repeat-my-header', function(request, response) {
+  if (!request.header.arguments) {
+    response.send("Bad request");
+    response.status(400);
+  }
+
+  if (request.header.arguments) {
+    response.send(request.header.arguments);
+  }  
+});
+
+server.get('repeat-my-cookie', function(request, response) {
+  if (!request.cookies) {
+    response.send("Bad request");
+    response.status(400);
+  }
+  if (request.cookies) {
+    response.send(request.cookies);
+  }
 });
 
 server.listen(8080);
